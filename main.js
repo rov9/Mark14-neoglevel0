dc3c4cd9e9a4076cdf965c7c66b59f7bd0f489e5
rov9 <duttasaurabh92@gmail.com>
2021-09-17 23:46:51 +0530

    html and js update

---
+++ main.js
@@ -0,0 +1,5 @@
+var initialPrice = document.querySelector(initial-price);
+var stocksQuantity=document.querySelector(stocks-quanity);
+var currentPrice=document.querySelector(current-price);
+var submitBtn=document.querySelector(submit-btn);
+var outputBox=document.querySelector();

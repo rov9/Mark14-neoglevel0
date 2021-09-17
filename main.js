d13f8761f07f97c55ac42560051c414ec814176f
rov9 <duttasaurabh92@gmail.com>
2021-09-18 01:15:26 +0530

    html and js update

--- main.js
+++ main.js
@@ -8,39 +8,47 @@ var outputBox=document.querySelector(output-box);
 submitBtn.addEventListener("click", submitHandler);
 
 function submitHandler(){
-    var ip= Number(initialPrice.value);
-    var qty= Number(stocksQuantity.value);
-    var curr = Number(currentPrice.value);
+    var ip= Number(initialPrice.value.toFixed(digits));
+    var qty= Number(stocksQuantity.value.toFixed(digits));
+    var curr = Number(currentPrice.value.toFixed(digits));
 
     calculateProfitAndLoss(ip, qty, curr);
 }
 
-if(ip==''||qty == ''||curr==''){
-    alert('Please fill out all Fields') 
-  }
+
 
 function calculateProfitAndLoss(initial, quantity, current){
     if (initial > current) {
         var loss = (initial - current) * quantity;
         var lossPercentage = (loss / initial) * 100;
-    
+        loss=loss.toFixed(2);
+        lossPercentage=lossPercentage.toFixed(2);
         showOutput(
           `Hey,Your loss is ${loss} and the percent is ${lossPercentage}%`
         );
-
+        function showOutput(message) {
+            outputBox.style.color = 'yellow';
+            outputBox.innerHTML = message;
+        }
+        
     } else if (current > initial) {
         var profit = (current - initial) * quantity;
         var profitPercentage = (profit / initial) * 100;
-    
+        profit=profit.toFixed(2);
+        profitPercentage=profitPercentage.toFixed(2);
         showOutput(
           `Hey,Your profit is ${profit} and the percent is ${profitPercentage}%`
         );
+
+        function showOutput(message) {
+            outputBox.style.color = 'yellow';
+            outputBox.innerHTML = message;
+        }
+        
       } else {
         showOutput(`No change in price of stock!`);
       }
     }
 
-    function showOutput(message) {
-        outputBox.innerHTML = message;
-    }
+    
 
